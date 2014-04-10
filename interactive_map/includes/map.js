98448f3e76ccc869b5542b01847220f3f1774233
russjr-fullsail <russjr@fullsail.edu>
2014-04-10 10:29:17 -0400

    finished map

--- interactive_map/includes/map.js
+++ interactive_map/includes/map.js
@@ -16,6 +16,25 @@ $(document).ready(function(){
         }
     });
 
+    $('a.dot').click(function(){
+        //alert( $(this).attr('city') );
+
+        $('a.dot').removeClass('selected');
+        $(this).addClass('selected');
+
+
+        var city = '.city_detail#' + $(this).attr('city');
+        var htmlCode = $(city).html();
+
+        $('.detail_container').fadeOut(500, function(){
+            $('.detail_container .city_detail').html(htmlCode);
+            $('.detail_container').fadeIn(500);
+        });
+
+
+
+    });
+
 
 
 
